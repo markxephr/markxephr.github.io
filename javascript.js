de5263b318e41009a7626f9d388f97635c24e2db
markxephr <markxephr@users.noreply.github.com>
2021-08-29 14:14:27 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function changeBlue (){
+	document.body.style.background = "blue";
+}
+
+function changeRed (){
+	document.body.style.background = "red";
+}
+
+function changeYellow (){
+	document.body.style.background = "yellow";
+}
